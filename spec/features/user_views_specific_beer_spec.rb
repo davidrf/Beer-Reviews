3e70a68c35830e70dcd665bac1f895e5f45b73b4
Rob Monk <robert.monk85@gmail.com>
2015-03-31 10:36:50 -0400

    New Tests

---
+++ spec/features/user_views_specific_beer_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'user views specific beer', %Q{
+  As a user,
+  I want to visit the details page of a specific beer
+  So that I can learn more about it } do
+  scenario 'goes to specific beer' do
+    beer = FactoryGirl.create(:beer)
+
+    visit root_path
+    click_on beer.name
+
+    expect(page).to have_content(beer.name)
+    expect(page).to have_content(beer.rating)
+  end
+end
