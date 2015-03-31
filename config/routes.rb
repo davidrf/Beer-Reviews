ee17e05899f4c9c6e792ff3bd980dd2fda17c3db
Ira Miller <imiller1985@gmail.com>
2015-03-31 13:59:22 -0400

    deleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,4 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :beers
-
-  resources :beers, only: [:index, :show] do
-    resources :reviews
-  end
-
 end
