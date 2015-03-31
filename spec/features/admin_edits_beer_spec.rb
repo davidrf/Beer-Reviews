0e584d2b515fe275a43f65fdefe70e4e7dfaeae8
ahite89 <ahite89@gmail.com>
2015-03-31 10:25:52 -0400

    First scenario admin edits revised

--- spec/features/admin_edits_beer_spec.rb
+++ spec/features/admin_edits_beer_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
-feature "Admin edits information for each individual beer page" do
+feature 'Admin edits information for each individual beer page', %Q{
+    As an admin, I want to be able to edit
+    the information for all beers cause I’m in charge
+  } do
+
+#Acceptance Criteria
+#I must be signed in
+#I must be able to get here through the details page for a specific beer
+#I must fill in the required fields
+#I must be presented with errors if I fill out the form incorrectly
+
 
   scenario 'successfully edit information' do
     admin = FactoryGirl.create(:admin_with_beer)
@@ -9,7 +19,7 @@ feature "Admin edits information for each individual beer page" do
     click_link 'Sign In'
     click_link 'Edit Beer'
     fill_in 'Name', with: 'Budweiser'
-    fill_in 'Style', with: 'Lager'
+    select 
     click_button 'Update Beer'
 
     expect(page).to have_content("Your beer has been updated!")
