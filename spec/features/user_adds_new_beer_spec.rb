43c32b67eab8848dced96c75e3bb4453511baa4e
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-03-31 17:19:46 -0400

    Implement first scenario of new beer feature

--- spec/features/user_adds_new_beer_spec.rb
+++ spec/features/user_adds_new_beer_spec.rb
@@ -1,55 +1,53 @@
-# require 'rails_helper'
-#
-# feature 'user posts new beer', %{
-#   As a user,
-#   I want to post information about a new beer
-#   So that I can share my beer love with the rest of the world
-# } do
-#   scenario 'user posts a new beer' do
-#     beer = FactoryGirl.build(:beer)
-#     user = FactoryGirl.create(:user)
-#
-#     visit root_path
-#     click_button "Sign In"
-#
-#     fill_in 'Email', with: user.email
-#     fill_in 'Password', with: user.password
-#
-#     click_button 'Log in'
-#
-#     click_button 'Add Beer'
-#
-#     fill_in 'Name', with: beer.name
-#     fill_in 'Style', with: beer.style_id
-#     click_button 'New Beer'
-# 
-#     expect(page).to have_content(beer.name)
-#   end
-#
-#   scenario 'unsuccessful post' do
-#     user = FactoryGirl.create(:user)
-#
-#     visit root_path
-#     click_button "Sign In"
-#
-#     fill_in 'Email', with: user.email
-#     fill_in 'Password', with: user.password
-#
-#     click_button 'Log in'
-#
-#     click_button 'Add Beer'
-#
-#     click_button 'New Beer'
-#
-#     expect(page).to have_content("Beer not added")
-#     expect(page).to have_content("Beer must have name")
-#     expect(page).to have_content("Beer must have a style")
-#   end
-#
-#   scenario 'user not signed in' do
-#
-#     visit root_path
-#
-#     expect(page).not_to have_button("Add Beer")
-#   end
-# end
+require 'rails_helper'
+
+feature 'user posts new beer', %{
+  As a user,
+  I want to post information about a new beer
+  So that I can share my beer love with the rest of the world
+} do
+  scenario 'user posts a new beer' do
+    beer = FactoryGirl.build(:beer)
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link "Sign In"
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+    click_link 'Add Beer'
+
+    fill_in 'Name', with: beer.name
+    select "Lager", from: "Style"
+    click_button 'Submit Beer'
+
+    save_and_open_page
+    expect(page).to have_content(beer.name)
+  end
+
+  scenario 'unsuccessful post' do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link "Sign In"
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
+    click_link 'Add Beer'
+
+    expect(page).to have_content("Beer not added")
+    expect(page).to have_content("Beer must have name")
+    expect(page).to have_content("Beer must have a style")
+  end
+
+  scenario 'user not signed in' do
+
+    visit root_path
+
+    expect(page).not_to have_button("Add Beer")
+  end
+end
