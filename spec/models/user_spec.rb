97d16df53b53d7013c831cef1082fd80f60b2404
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-04-01 10:39:23 -0400

    Revise according to Hound comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe User, type: :model do
   it { should have_many :beers }
 
   it { should have_valid(:email).when("dabomb@gmail.com", "hi@yahoo.com") }
-  it { should_not have_valid(:email).when(nil, "", "dabomb", "dabomb@", "dabomb@gmail", "@gmail.com", ".com") }
+  it { should_not have_valid(:email).when(
+    nil, "", "dabomb", "dabomb@", "dabomb@gmail", "@gmail.com", ".com") }
 
   it { should have_valid(:password).when("password") }
   it { should_not have_valid(:password).when(nil, "") }
