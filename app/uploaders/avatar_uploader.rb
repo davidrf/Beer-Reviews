63dcbdd1566854fe0fb2702305ba6ef5197ac36e
ahite89 <ahite89@gmail.com>
2015-04-02 14:45:26 -0400

    Fixed houndci issues

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,16 +1,15 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   if Rails.env.production? || Rails.env.development?
-   storage :fog
- else
-   storage :file
- end
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -21,10 +20,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # 'default_avatar.png' #rails will look at 'app/assets/images/default_avatar.png'
+  # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # 'default_avatar.png'
   # end
 
   # Process files as they are uploaded:
@@ -46,9 +44,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # Avoid using model.id or version_name here, see uploader/store.rb for details
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
