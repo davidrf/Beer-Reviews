ca4abc218a33ab5398c3c78b3408579da18288ad
ahite89 <ahite89@gmail.com>
2015-04-03 14:22:28 -0400

    Fixed hound comments

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,17 +1,15 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-
   if Rails.env.production? || Rails.env.development?
     storage :fog
   else
     storage :file
-    enable_processing = false
   end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url(*args)
+  def default_url
     ActionController::Base.helpers.asset_path("fallback/default.jpg")
   end
 end
