0f72116b03d161ffdf1b008c3e6db969bfa51854
Rob Monk <robert.monk85@gmail.com>
2015-04-07 12:52:18 -0400

    Fix hound errors

--- spec/features/user_review_submits_email_spec.rb
+++ spec/features/user_review_submits_email_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 feature "As a user,I want to review a beer,
-so I could let the beer owner know I hate his/her beer",%{
+so I could let the beer owner know I hate his/her beer", %{
   [ ] I should be able post a review
   [ ] It must send an email to the owner of the beer
 } do
