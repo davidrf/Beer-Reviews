3670716ffda52184c5a5045af14df4db637d66c7
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-04-08 17:16:17 -0400

    Fix Hound Errors

--- app/assets/javascripts/edit_review.js
+++ app/assets/javascripts/edit_review.js
@@ -1,34 +1,34 @@
 $(".edit_button").click(function (event) {
   event.preventDefault();
   var href = this.href.split("/");
-  var review_id = href[href.length - 2];
+  var ReviewId = href[href.length - 2];
   if (this.text == "Edit Review") {
-    var rating_string = $('#review_rating' + review_id).text().split(" ");
-    var description = $('#review_description' + review_id).text();
-    var rating = rating_string[rating_string.length - 1][0];
+    var RatingString = $("#review_rating" + ReviewId).text().split(" ");
+    var description = $("#review_description" + ReviewId).text();
+    var rating = RatingString[RatingString.length - 1][0];
 
-    $('.review_item' + review_id).remove();
-    $('.column#rating' + review_id).append(select_box(rating, review_id));
-    $('.column#description' + review_id).append(text_box(description, review_id));
+    $(".review_item" + ReviewId).remove();
+    $(".column#rating" + ReviewId).append(SelectBox(rating, ReviewId));
+    $(".column#description" + ReviewId).append(TextBox(description, ReviewId));
     this.text = "Resubmit Review";
   } else {
-    var new_rating = $("#edit_rating" + review_id).val();
-    var new_description = $("#edit_description" + review_id).val();
-    $('#edit_description' + review_id).remove();
-    $('#edit_rating' + review_id).remove();
-    $('.column#rating' + review_id).append(reload_rating(new_rating, review_id));
-    $('.column#description' + review_id).append(reload_description(new_description, review_id));
+    var NewRating = $("#edit_rating" + ReviewId).val();
+    var NewDescription = $("#edit_description" + ReviewId).val();
+    $("#edit_description" + ReviewId).remove();
+    $("#edit_rating" + ReviewId).remove();
+    $(".column#rating" + ReviewId).append(reload_rating(NewRating, ReviewId));
+    $(".column#description" + ReviewId).append(reload_description(NewDescription, ReviewId));
     this.text = "Edit Review"
     $.ajax({
       type: "PUT",
-      url: "/reviews/" + review_id,
-      dataType: 'json',
-      data: { review: { rating: new_rating, description: new_description } }
+      url: "/reviews/" + ReviewId,
+      dataType: "json",
+      data: { review: { rating: NewRating, description: NewDescription } }
     })
   }
 });
 
-function select_box(value, id){
+function SelectBox(value, id){
   var html = "<select class='.review_item" + id + "' id='edit_rating" + id + "'>"
   for (var i = 1; i < 11; i++) {
     if (Number(value) === i) {
@@ -41,20 +41,20 @@ function select_box(value, id){
   return html;
 };
 
-function text_box(details, id){
+function TextBox(details, id){
   var html = "<textarea class='.review_item" + id
   html += "' id='edit_description" + id + "'>" + details + "</textarea>"
   return html;
 };
 
-function reload_rating(new_rating, id){
+function reload_rating(NewRating, id){
   var html = "<p class='review_item" + id + "' id= 'review_rating" + id + "'>"
-  html += "Rating: " + new_rating + "/10</p>"
+  html += "Rating: " + NewRating + "/10</p>"
   return html;
 };
 
-function reload_description(new_description, id){
+function reload_description(NewDescription, id){
   var html = "<p class='review_item" + id + "' id= 'review_description" + id + "'>"
-  html += new_description + "</p>"
+  html += NewDescription + "</p>"
   return html;
 };
